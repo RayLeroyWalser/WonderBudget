7ce74c20308a0409747fd65f18a67fb71967e54e
rubeus90 <AndroidGirl90>
2014-10-11 01:23:37 +0200

    Delete one or multiple Transaction with Contextual Action Bar

--- mobile/src/main/java/com/android/rubeus/wonderbudget/TransactionListFragment.java
+++ mobile/src/main/java/com/android/rubeus/wonderbudget/TransactionListFragment.java
@@ -4,9 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
@@ -14,6 +19,7 @@ import com.android.rubeus.wonderbudget.CustomAdapter.TransactionLineAdapter;
 import com.android.rubeus.wonderbudget.DBHandler.DatabaseHandler;
 import com.android.rubeus.wonderbudget.Entity.Transaction;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionListFragment extends Fragment {
@@ -53,6 +59,66 @@ public class TransactionListFragment extends Fragment {
                 intent.putExtra("typeOfDialog", TransactionActionActivity.VIEW_TRANSACTION);
                 intent.putExtra("transactionId", adapter.getItemId(position));
                 startActivityForResult(intent, TransactionActionActivity.VIEW_TRANSACTION);
+            }
+        });
+
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            private int number = 0;
+            private ArrayList<Transaction> listToDelete = new ArrayList<Transaction>();
+
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position,
+                                                  long id, boolean checked) {
+                // Here you can do something when items are selected/de-selected,
+                // such as update the title in the CAB
+                if(checked){
+                    number++;
+                    listToDelete.add(db.getTransaction((int) id));
+                }
+                else{
+                    number--;
+                    listToDelete.remove(db.getTransaction((int) id));
+                }
+                mode.setTitle(number + " selected");
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                // Respond to clicks on the actions in the CAB
+                switch (item.getItemId()) {
+                    case R.id.deleteTransaction:
+                        //TODO: delete transaction
+                        for(int i=0; i<listToDelete.size(); i++){
+                            db.deleteTransaction(listToDelete.get(i));
+                        }
+                        adapter.refresh(db.getAllTransactions());
+                        mode.finish(); // Action picked, so close the CAB
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                // Inflate the menu for the CAB
+                MenuInflater inflater = mode.getMenuInflater();
+                inflater.inflate(R.menu.transaction_list_context_menu, menu);
+                return true;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                // Here you can make any necessary updates to the activity when
+                // the CAB is removed. By default, selected items are deselected/unchecked.
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                // Here you can perform updates to the CAB due to
+                // an invalidate() request
+                return false;
             }
         });
 
