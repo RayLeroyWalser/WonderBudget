f4b3f256da1e0df41134e1c0b7e3d40286ff2c29
rubeus90 <AndroidGirl90>
2014-10-28 23:29:29 +0100

    Minor appearance change : color for OverviewFragment

--- mobile/src/main/java/com/android/rubeus/wonderbudget/OverviewFragment.java
+++ mobile/src/main/java/com/android/rubeus/wonderbudget/OverviewFragment.java
@@ -44,7 +44,7 @@ public class OverviewFragment extends Fragment {
         totalAmount = (TextView) view.findViewById(R.id.totalAmount);
         realAmount = (TextView) view.findViewById(R.id.realAmount);
         ImageButton addButton = (ImageButton) view.findViewById(R.id.addButton);
-        ((GradientDrawable)addButton.getBackground()).setColor(getResources().getColor(R.color.pink));
+        ((GradientDrawable)addButton.getBackground()).setColor(getResources().getColor(R.color.blue));
 
         totalAmount.setText(db.getTotalAmount()+"");
         realAmount.setText(db.getRealAmount()+"");
